ea329902199fe42f57cf71d337396cc631ac310c
Jiahao Cui <k20108456@kcl.ac.uk>
2022-12-08 19:20:03 +0000

    update

---
+++ .github/things/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.validators import RegexValidator,MaxValueValidator,MinValueValidator
+
+class Thing(models.Model):
+    name = models.CharField(
+        max_length = 30,
+        unique = True,
+        blank = False,
+    )
+
+    descriptions = models.CharField(
+        max_length = 120,
+        unique = False,
+        blank = True,
+    )
+
+    quantity = models.IntegerField(
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0),
+        ]
+    )
